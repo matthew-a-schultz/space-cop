17973c776b9da1b9f463e2feb8d726fa0f8bb4d4
Matthew A Schultz <matthewschultz@me.com>
2025-08-14 18:16:39 -0600

    Added save hook and additional data changes for save and load

--- addons/event_editor/classes/graph_node_extended.gd
+++ addons/event_editor/classes/graph_node_extended.gd
@@ -4,10 +4,11 @@ class_name GraphNodeExtended
 
 enum SlotType {START, OBJECTS, ACTIVE, POSITION, FINISHED}
 enum Side {NONE, LEFT, RIGHT, BOTH}
+var graph_node_resource: GraphNodeResource = GraphNodeResource.new()
 var slots: Array[Array]
 var slot_types: Array[SlotType]
 var slot_sides: Array[Side]
-var slot_status: Dictionary [SlotType, World.SlotStatus]
+var slot_status: Dictionary [SlotType, EventEditorConfig.SlotStatus]
 var _slot_output_lookup: Array[Array]
 var _slot_type_lookup: Dictionary[SlotType, Variant.Type] = {
 	SlotType.START: TYPE_BOOL,
@@ -17,6 +18,13 @@ var _slot_type_lookup: Dictionary[SlotType, Variant.Type] = {
 	SlotType.FINISHED: TYPE_BOOL,
 }
 var _editor: Editor
+
+func _init() -> void:
+	position_offset_changed.connect(_position_offset_changed)
+	graph_node_resource.name = name
+
+func _position_offset_changed() -> void:
+	graph_node_resource.position_offset = position_offset
 
 func update_slots(new_slots: Array[Array]) -> void:
 	slots = new_slots
@@ -40,13 +48,13 @@ func set_slot_left(slot_index: int, type: Variant.Type) -> void:
 	if type != TYPE_NIL:
 		set_slot_enabled_left(slot_index, true)
 		set_slot_type_left(slot_index, type)
-		set_slot_color_left(slot_index, World.node_type_color[type])
+		set_slot_color_left(slot_index, EventEditorConfig.node_type_color[type])
 	
 func set_slot_right(slot_index: int, type: Variant.Type) -> void:
 	if type != TYPE_NIL:
 		set_slot_enabled_right(slot_index, true)
 		set_slot_type_right(slot_index, type)
-		set_slot_color_right(slot_index, World.node_type_color[type])
+		set_slot_color_right(slot_index, EventEditorConfig.node_type_color[type])
 
 func connect_node(from_slot_index: int, to_slot_index: int, to_node: GraphNodeExtended) -> Error:
 	var slot_output: Array = [to_slot_index, to_node]
@@ -65,8 +73,5 @@ func update_slot_value(value: Variant, from_slot_index: int) -> void:
 func set_slot_value(value: Variant, to_slot_index: int) -> void:
 	return
 
-func save_node() -> Dictionary:
-	return {}
-
-func load_node(new_slot_data: Dictionary) -> void:
+func load_save_data(save_data: Dictionary) -> void:
 	return
