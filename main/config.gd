1f3f4bfd329a45ebda9451e48774a83af93f3a49
Matthew A Schultz <matthewschultz@me.com>
2025-08-20 22:00:43 -0600

    New global variables

---
+++ main/config.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name Config
+
+const LIGHT_TIME_TO_CHANGE: int = 5
+enum Direction {NORTH, EAST, SOUTH, WEST}
+enum RoadSide {TOP, BOTTOM}
+enum Collisions {TRAFFIC_LIGHT = 64, INTERSECTION = 128}
