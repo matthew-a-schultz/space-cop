e33f0c03d66ad55ce8f4e77c9bde65cd6a08478d
Matthew A Schultz <matthewschultz@me.com>
2025-08-26 15:24:25 -0600

    More adjustments

--- addons/scenario_editor/scenario_editor_plugin.gd
+++ addons/scenario_editor/scenario_editor_plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 class_name ScenarioEditor
 var scenario_editor_scene: Editor
-static var scene_editor: SceneEditor
+static var game: Game
 
 const save_path = "res://addons/scenario_editor/temp/scene_editor_default.tscn"
 
@@ -15,25 +15,18 @@ func _enter_tree() -> void:
 	var world_editor_packed_scene: PackedScene = ResourceLoader.load("res://addons/scenario_editor/editor/scene_editor_default.tscn")
 	ResourceSaver.save(world_editor_packed_scene, save_path)
 	EditorInterface.open_scene_from_path(save_path)
-	scene_editor = EditorInterface.get_edited_scene_root()
+	game = EditorInterface.get_edited_scene_root()
 
 func _exit_tree() -> void:
 	remove_control_from_bottom_panel(scenario_editor_scene)
 
 func _mission_editor_pressed() -> void:
 	EditorInterface.open_scene_from_path(save_path)
-	scene_editor = EditorInterface.get_edited_scene_root()
+	game = EditorInterface.get_edited_scene_root()
 	
 func _scene_changed(scene_root: Node) -> void:
-	if scene_root is SceneEditor:
+	if scene_root is Game:
 		make_bottom_panel_item_visible(scenario_editor_scene)
 
 func _get_plugin_name() -> String:
 	return "ScenarioEditor"
-
-func add_world_object(object: WorldObject) -> void:
-	var name: String = object.name
-	scene_editor.ui_objects.add_child(object)
-	object.owner = scene_editor
-	object.name = name
-	
