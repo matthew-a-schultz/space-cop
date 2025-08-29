6cbeaf49fb9d293e3eb47ecbd091379343c81530
Matthew A Schultz <matthewschultz@me.com>
2025-08-29 16:28:56 -0600

    Use path instead of _file_path

--- addons/scenario_editor/editor/graph_nodes/graph_node_play_audio.gd
+++ addons/scenario_editor/editor/graph_nodes/graph_node_play_audio.gd
@@ -8,12 +8,12 @@ var _playing: bool = false
 @export var _choose_file_button: Button
 @export var _file_path_label: Label
 var _audio_stream: AudioStream
-var _file_path: String
+var path: String
 
 func _ready() -> void:
 	graph_node_resource.type = ScenarioEditorConfig.GraphNodeType.PLAY_AUDIO
 	graph_node_resource.save_data = {
-		Slot.FILE_PATH: _file_path,
+		Slot.FILE_PATH: path,
 	}
 	update_slots([
 		[TYPE_BOOL, Side.LEFT],
@@ -35,12 +35,12 @@ func get_input(value: Variant, to_slot_index: int) -> void:
 	match to_slot_index:
 		Slot.PLAY:
 			_playing = true
-			Game.audio.play_from_graph_node(_file_path, self)
+			Game.audio.play_from_graph_node(self)
 
 func _file_selected(file_path: String) -> void:
 	graph_node_resource.save_data[Slot.FILE_PATH] = file_path
 	_file_path_label.text = graph_node_resource.save_data[Slot.FILE_PATH]
-	_file_path = graph_node_resource.save_data[Slot.FILE_PATH]
+	path = graph_node_resource.save_data[Slot.FILE_PATH]
 
 func load_save_data(save_data: Dictionary) -> void:
 	_file_selected(save_data[Slot.FILE_PATH])
