41194ddec9470a70e27cf94c78e35d26c3f52e37
Matthew A Schultz <matthewschultz@me.com>
2025-08-29 16:23:11 -0600

    Created audio queue

--- main/scenes/audio.gd
+++ main/scenes/audio.gd
@@ -7,12 +7,19 @@ var _audio_stream_player2d_queue: Array[GraphNodePlayAudio]
 func _ready() -> void:
 	audio_stream_player2d.finished.connect(_audio_stream_player2d_finished)
 
-func play_from_graph_node(file_path: String, graph_node: GraphNodePlayAudio) -> void:
+func play_from_graph_node(graph_node: GraphNodePlayAudio) -> void:
 	_audio_stream_player2d_queue.append(graph_node)
-	var audio_stream: AudioStream = ResourceLoader.load(file_path)
+	_play_queue()
+
+func _play_queue() -> void:
+	if audio_stream_player2d.playing or _audio_stream_player2d_queue.size() == 0:
+		return
+	var graph_node: GraphNodePlayAudio = _audio_stream_player2d_queue.front() as GraphNodePlayAudio
+	var audio_stream: AudioStream = ResourceLoader.load(graph_node.path)
 	audio_stream_player2d.stream = audio_stream
 	audio_stream_player2d.play()
 
 func _audio_stream_player2d_finished() -> void:
 	var graph_node: GraphNodePlayAudio = _audio_stream_player2d_queue.pop_front()
 	graph_node.finished_playing()
+	_play_queue()
